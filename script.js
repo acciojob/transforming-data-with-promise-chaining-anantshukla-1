7a67edcf638cf0ddcd8429fabe26d4545b2e015e
anantshukla-1 <shukla18anant9839@gmail.com>
2026-05-23 16:50:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('btn').onclick = function(){
 
 	let firstPromise = new Promise((resolve, reject) =>{
 		setTimeout(()=>{
-			document.getElementById('output').innerHTML = "Result" + number;
+			document.getElementById('output').innerHTML = "Result: " + number;
 			resolve(number);
 		},2000);
 		
@@ -14,7 +14,7 @@ document.getElementById('btn').onclick = function(){
 		return new Promise((resolve, reject)=>{
 			setTimeout(()=>{	
 			let mlty = result * 2;
-			document.getElementById('output').innerHTML = "Result" + mlty;	
+			document.getElementById('output').innerHTML = "Result: " + mlty;	
 			resolve(mlty);
 		},2000)
 		});
@@ -23,7 +23,7 @@ document.getElementById('btn').onclick = function(){
 		return new Promise((resolve, reject)=>{
 			setTimeout(()=>{	
 			let sub = result - 3 ;
-			document.getElementById('output').innerHTML = "Result" + sub;	
+			document.getElementById('output').innerHTML = "Result: " + sub;	
 			resolve(sub);
 		},1000)
 		});
@@ -32,7 +32,7 @@ document.getElementById('btn').onclick = function(){
 		return new Promise((resolve, reject)=>{
 			setTimeout(()=>{	
 			let divd = result / 2 ;
-			document.getElementById('output').innerHTML = "Result" + divd;	
+			document.getElementById('output').innerHTML = "Result: " + divd;	
 			resolve(divd);
 		},1000);
 		});
@@ -41,7 +41,7 @@ document.getElementById('btn').onclick = function(){
 		return new Promise((resolve, reject)=>{
 			setTimeout(()=>{	
 			let add = result + 10 ;
-			document.getElementById('output').innerHTML = "Final Result" + add;	
+			document.getElementById('output').innerHTML = "Final Result: " + add;	
 			resolve(add);
 		},1000)
 		});
